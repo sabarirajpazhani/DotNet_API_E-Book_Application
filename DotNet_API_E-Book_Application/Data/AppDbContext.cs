9f473640929775d4ee79db52b60d6f943a18c6c5
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-19 19:45:51 +0530

    Create App DB Context

---
+++ DotNet_API_E-Book_Application/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DotNet_API_E_Book_Application.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_API_E_Book_Application.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+
+}
