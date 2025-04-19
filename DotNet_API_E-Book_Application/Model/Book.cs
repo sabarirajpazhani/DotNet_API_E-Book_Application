3c5c3c969c47512b6ce512433c09b5b581941813
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-19 19:26:34 +0530

    Add project files.

---
+++ DotNet_API_E-Book_Application/Model/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNet_API_E_Book_Application.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String Description { get; set; }
+    }
+}
